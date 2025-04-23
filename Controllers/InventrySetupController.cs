f215cd975434077b98188127cec7a2bb296efaac
OwaisAslam911 <owaisaslams911@gmail.com>
2025-04-23 11:21:08 -0700

    template integerated

---
+++ Controllers/InventrySetupController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesPro.Controllers
+{
+    public class InventrySetupController : Controller
+    {
+        public IActionResult CategorySetup()
+        {
+            return View();
+        }
+        public IActionResult BrandSetup()
+        {
+            return View();
+        }
+        public IActionResult UnitSetup()
+        {
+            return View();
+        }
+        public IActionResult ItemSetup()
+        {
+            return View();
+        }
+    }
+}
