1d89f2f531566344626cb467f841be31636a8312
OwaisAslam911 <owaisaslams911@gmail.com>
2025-05-10 09:47:57 -0700

    owais

--- Models/Sale.cs
+++ Models/Sale.cs
@@ -56,6 +56,7 @@ public class SaleBillViewModel
     public decimal NetTotal => GrossTotal + AddCharges - Discount;
 
     public List<SaleItem> SaleItems { get; set; }
+
 }
 
 public class SaleItem
